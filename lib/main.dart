4179d7da06645717b40722f0ecfc246695e95b9b
Jonathan Duron <yonadur17@gmail.com>
2020-06-12 11:42:25 +0200

    first version

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'data/moor_database.dart';
+import 'ui/homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+      builder: (_) => AppDatabase(),
+      child: MaterialApp(
+        title: 'Material App',
+        home: HomePage(),
+      ),
+    );
+  }
+}
